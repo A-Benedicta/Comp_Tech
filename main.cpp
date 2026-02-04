699004e53c08d0334ed9a6bb5ee65897244507ab
A-Benedicta <benedictaagbodanseven@gmail.com>
2026-02-04 13:03:27 +0000

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+// QUESTION 11:
+// Explain the difference between the increment and decrement operators in C++.
+// Give an example of each.
+
+
+int main() {
+    cout<< "Increment (++) adds 1."<<endl;
+    cout<< "Example: int x = 5; x++;  // x becomes 6"<<endl;
+    
+    
+    cout<< "Decrement (--) subtracts 1."<<endl;
+    cout<< "Example: int y = 5; y--;  // y becomes 4"<<endl;
+    
+    return 0;
+}
